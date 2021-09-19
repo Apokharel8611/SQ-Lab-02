9cbaf42c82867faaa0f3d764249c80506ede122d
Arpan Pokharel <Apokharel8611@conestogac.on.ca>
2021-09-18 20:02:38 -0400

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SQ Lab 02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SQ Lab 02")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67429ea5-48f0-4cf4-bc3f-a7ed2299c747")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+public class Hypotenus
+{
+    public int CalculateHypotenus(int num1, int num2)
+    {
+        return 0;
+    }
+}
+
+namespace SQ_Lab_02
+{
+    [TestClass]
+    public class TriangleAll
+    {
+        [TestMethod]
+        public void Hypotenus()
+        {
+            Hypotenus obj = new Hypotenus();
+            double result = obj.CalculateHypotenus(4, 3);
+            Assert.AreEqual(5, result);
+        }
+    }
+}
