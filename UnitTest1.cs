633f8a76d8c70b4f20fbfea91938ba8192df22b8
Arpan Pokharel <Apokharel8611@conestogac.on.ca>
2021-09-18 20:17:18 -0400

    This is the third test, in which area was added. But the value is wrong. Expected 6, return 0.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,12 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-public class Hypotenus
+public class Triangle
 {
     public double CalculateHypotenus(int num1, int num2)
     {
         double sum = (num1 * num1) + (num2 * num2);
         return sum/5;                   //I tried using sqrt(sum), but it failed so I used this.
+    }
+
+    public double CalculateArea(int num1, int num2)
+    {
+       
+        return 0;    
     }
 }
 
@@ -18,9 +24,17 @@ namespace SQ_Lab_02
         [TestMethod]
         public void Hypotenus()
         {
-            Hypotenus obj = new Hypotenus();
+            Triangle obj = new Triangle();
             double result = obj.CalculateHypotenus(4, 3);
             Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Area()
+        {
+            Triangle obj = new Triangle();
+            double result = obj.CalculateArea(4, 3);
+            Assert.AreEqual(6, result);
         }
     }
 }
